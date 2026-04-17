391a6a2772a4e031c7b3495094365d80d375beb9
Chu Duc Nhat Minh <chuducnhatminh@gmail.com>
2026-04-18 00:43:36 +0700

    cap nhat server.js phan app.use(cors())

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,12 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-app.use(
-  cors({
-    origin: FRONTEND_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
